806c8d00b74c6b95317a4db776f399cff43b199b
LukeOfficial <e87624273@gmail.com>
2024-01-17 08:34:22 +0400

    Update sortBy.js

--- js/sortBy.js
+++ js/sortBy.js
@@ -1,3 +1,7 @@
 var sortByDropdown = document.getElementById("sortByDropdown");
 var sortByOwner = document.getElementById("owner");
 var sortByCommunity = document.getElementById("community");
+
+sortByDropdown.addEventListener("change",function(){
+  if (sortByDropdown.value == "all") then
+});
