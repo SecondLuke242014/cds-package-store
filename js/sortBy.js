bc97018ddfbaf7f9a8d2627661a7489bea0f85eb
LukeOfficial <e87624273@gmail.com>
2024-01-17 08:42:54 +0400

    Update sortBy.js

--- js/sortBy.js
+++ js/sortBy.js
@@ -3,5 +3,14 @@ var sortByOwner = document.getElementById("owner");
 var sortByCommunity = document.getElementById("community");
 
 sortByDropdown.addEventListener("change",function(){
-  if (sortByDropdown.value == "all") then
+  if (sortByDropdown.value == "all") {
+    sortByOwner.style.display = "block";
+    sortByCommunity.style.display = "block";
+  } else if (sortByDropdown.value == "owner") {
+    sortByOwner.style.display = "block";
+    sortByCommunity.style.display = "none";
+  } else if (sortByDropdown.value == "community") {
+    sortByOwner.style.display = "none";
+    sortByCommunity.style.display = "block";
+  };
 });
